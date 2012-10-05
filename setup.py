53284844dfbbf1693404b8ba2ef63a7d524f23f9
Philipp <fsfeel@gmail.com>
2012-10-06 02:15:13 +0400

    increment version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setuptools.setup(
 	name = 'bold',
-	version = '0.2.0',
+	version = '0.2.1',
 	description = 'A software build automation tool',
 	long_description = 'A software build automation tool',
 	author = 'Philipp Saveliev',
